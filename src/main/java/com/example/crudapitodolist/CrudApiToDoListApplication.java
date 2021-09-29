ed205ce490d7d2eb590c39e38f78b8c9edf22be7
Joseph Myrick <joseph.l.myrick2.mil@swf.army.mil>
2021-09-29 10:07:44 -0500

    Initial commit.

---
+++ src/main/java/com/example/crudapitodolist/CrudApiToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudapitodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiToDoListApplication.class, args);
+	}
+
+}
